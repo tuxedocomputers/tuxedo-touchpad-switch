13caaaff8eda33510bab1a3dc829e7d4b6e60ab9
Werner Sembach <wse@tuxedocomputers.com>
2020-12-18 10:34:02 +0100

    Working prototype

--- tuxedo-touchpad-switch-deamon.cpp
+++ tuxedo-touchpad-switch-deamon.cpp
@@ -5,9 +5,12 @@
 #include <stdio.h>
 #include <libudev.h>
 #include <string.h>
+#include <signal.h>
 
 #include <vector>
 #include <string>
+
+#include <gio/gio.h>
 
 const char *bus_str(int bus);
 
@@ -67,49 +70,110 @@ static int get_touchpad_hidraw_devices(std::vector<std::string> *devnodes) {
     return devnodes->size();;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <enable|disable>\n", argv[0]);
-        return EXIT_SUCCESS;
+void send_events_handler(GSettings *settings, const char* key, gpointer user_data) {
+    const gchar *send_events_string = g_settings_get_string(settings, key);
+    if (!send_events_string) {
+        perror("g_settings_get_string");
+        return;
     }
     
     std::vector<std::string> devnodes;
     int touchpad_count = get_touchpad_hidraw_devices(&devnodes);
     if (touchpad_count < 0) {
         perror("get_touchpad_hidraw_devices");
-        return EXIT_FAILURE;
+        return;
     }
     if (touchpad_count == 0) {
         printf("No compatible touchpads found.\n");
-        return EXIT_SUCCESS;
+        return;
     }
     
-    int result = EXIT_SUCCESS;
     for (auto it = devnodes.begin(); it != devnodes.end(); ++it) {
         int hidraw = open((*it).c_str(), O_RDWR|O_NONBLOCK);
         if (hidraw < 0) {
             perror("open");
-            result = EXIT_FAILURE;
             continue;
         }
 
         char buffer[2] = {0x07, 0x03};
-        if (argv[1][0] == 'd') {
+        if (send_events_string[0] == 'd') {
             buffer[1] = 0x00;
         }
         int result = ioctl(hidraw, HIDIOCSFEATURE(sizeof(buffer)/sizeof(buffer[0])), buffer);
         if (result < 0) {
             perror("ioctl");
             close(hidraw);
-            result = EXIT_FAILURE;
             continue;
-        }
-        else {
-            printf("ioctl returned: %d\n", result);
         }
 
         close(hidraw);
     }
+}
+
+void gracefull_exit(int signum) {
+    GSettings *settings_touchpad = g_settings_new("org.gnome.desktop.peripherals.touchpad");
+    if (!settings_touchpad) {
+        perror("g_settings_new");
+        exit(EXIT_FAILURE);
+    }
     
-    return result;
+    std::vector<std::string> devnodes;
+    int touchpad_count = get_touchpad_hidraw_devices(&devnodes);
+    if (touchpad_count < 0) {
+        perror("get_touchpad_hidraw_devices");
+        exit(EXIT_FAILURE);
+    }
+    if (touchpad_count == 0) {
+        printf("No compatible touchpads found.\n");
+        exit(EXIT_SUCCESS);
+    }
+    
+    for (auto it = devnodes.begin(); it != devnodes.end(); ++it) {
+        int hidraw = open((*it).c_str(), O_RDWR|O_NONBLOCK);
+        if (hidraw < 0) {
+            perror("open");
+            continue;
+        }
+
+        char buffer[2] = {0x07, 0x03};
+        int result = ioctl(hidraw, HIDIOCSFEATURE(sizeof(buffer)/sizeof(buffer[0])), buffer);
+        if (result < 0) {
+            perror("ioctl");
+            close(hidraw);
+            continue;
+        }
+
+        close(hidraw);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char *argv[]) {
+    signal(SIGINT, gracefull_exit);
+    signal(SIGTERM, gracefull_exit);
+    
+    GSettings *settings_touchpad = g_settings_new("org.gnome.desktop.peripherals.touchpad");
+    if (!settings_touchpad) {
+        perror("g_settings_new");
+        return EXIT_FAILURE;
+    }
+    
+    // sync on start
+    send_events_handler(settings_touchpad, "send-events", NULL);
+    
+    if (g_signal_connect(settings_touchpad, "changed::send-events", G_CALLBACK(send_events_handler), NULL) < 1) {
+        perror("g_signal_connect");
+        return EXIT_FAILURE;
+    }
+    
+    GMainLoop *app = g_main_loop_new(NULL, TRUE);
+    if (!app) {
+        perror("g_main_loop_new");
+        return EXIT_FAILURE;
+    }
+    
+    g_main_loop_run(app);
+    // g_main_loop_run should not return
+    perror("g_main_loop_run");
+    return EXIT_FAILURE;
 }
